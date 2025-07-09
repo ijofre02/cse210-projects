c0ec28aa70cfb66b8d8841796bc4205b1e14d0b2
ijofre02 <ignacio.d.jofre@gmail.com>
2025-07-09 13:31:32 -0300

    Exercise 3 assigment

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // This is the firs part
+        //Console.Write("What is the magic number? ");
+        //string magicNumberStr = Console.ReadLine();
+        //int magicNumber = int.Parse(magicNumberStr);
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int gessNumber = -1;
+
+        while (magicNumber != gessNumber)
+        {
+            Console.Write("What is your gess? ");
+            gessNumber = int.Parse(Console.ReadLine()); // all in one line
+
+            if (magicNumber < gessNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (magicNumber > gessNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!!");
+            }
+        }
+
     }
 }
