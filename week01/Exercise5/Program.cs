176dd93cc265a7bee3e97e6ca8565e9f617d2868
ijofre02 <ignacio.d.jofre@gmail.com>
2025-07-15 11:55:25 -0300

    The lasth

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int userNumber = PrompUserNumber();
+
+        int squeredNumber = SquareNumber(userNumber);
+        DisplayResult(userName, squeredNumber);
+
+    }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PrompUserNumber()
+    {
+        Console.Write("Please enter your favorite number:");
+        int FavoriteNumber = int.Parse(Console.ReadLine());
+        return FavoriteNumber;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the squere of  your number is {square}");
     }
 }
